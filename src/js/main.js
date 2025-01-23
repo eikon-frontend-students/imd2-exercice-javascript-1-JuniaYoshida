2540054bdd069eef919183414d127dc4b7df375a
JuniaYoshida <Junia.Yoshida@studentfr.ch>
2025-01-23 10:16:14 +0100

    add

--- src/js/main.js
+++ src/js/main.js
@@ -10,13 +10,27 @@ boxToggleColor.addEventListener("click", function () {
   boxToggleColor.classList.toggle("is-active");
 });
 
-var boxTrigger = document.querySelector(".box.trigger");
+var boxClick = document.querySelector(".box.trigger");
+var boxNone = document.querySelector(".box.target");
 
-boxTrigger.addEventListener("click", function () {
-  boxTrigger.classList.remove("is-active");
-});
+function ToggleChange() {
+  boxNone.classList.toggle("is-active");
+}
+boxClick.addEventListener("click", ToggleChange);
 
-var boxTarget = document.querySelector(".box.target");
+var boxTriggerAll = document.querySelector(".box.trigget-all");
+
+function ToggleChange() {
+  boxNone.classList.toggle("is-active");
+}
+boxTriggerAll.classList.toggle("click", ToggleChange);
+// boxTrigger.addEventListener("click", function () {
+//   boxTrigger.classList.remove("is-active");
+// });
+// boxTarget.addEventListener("click", function () {
+//   boxTrigger.classList.remove("is-active");
+//   boxTarget.classList.add("is-active");
+// });
 
 // var onClick =
 
